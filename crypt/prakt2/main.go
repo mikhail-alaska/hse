20d957af43543d858901aa0b8bf647504fdc1508
mikhail-alaska <mishartem2004@gmail.com>
2025-02-23 14:22:30 +0300

    Scripted auto-commit on change (2025-02-23 14:22:30) by gitwatch.sh

--- crypt/prakt2/main.go
+++ crypt/prakt2/main.go
@@ -422,9 +422,9 @@ func main() {
 	fmt.Printf("Найдено точек: %d\n", len(pts))
 	for _, pt := range pts {
 		if pt.Infinity {
-			fmt.Printf("Infinity, ")
+			fmt.Println("Infinity")
 		} else {
-			fmt.Printf("(%s, %s), ", pt.X.String(), pt.Y.String())
+			fmt.Printf("(%s, %s)\n", pt.X.String(), pt.Y.String())
 		}
 	}
 
