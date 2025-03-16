3dcf68e05f8845d43ccc3070f6508af8908b37b7
mikhail-alaska <mishartem2004@gmail.com>
2025-03-16 17:03:08 +0300

    Scripted auto-commit on change (2025-03-16 17:03:08) by gitwatch.sh

--- crypt/prakt4/main.go
+++ crypt/prakt4/main.go
@@ -8,7 +8,6 @@ import (
 	"time"
 )
 
-// modPow вычисляет (a^x) mod n с помощью быстрого возведения в степень.
 func modPow(a, x, n int) int {
 	result := 1
 	for x > 0 {
@@ -21,12 +20,10 @@ func modPow(a, x, n int) int {
 	return result
 }
 
-// checkPrimeFerma проверяет число по малой теореме Ферма.
 func checkPrimeFerma(n int) bool {
 	return modPow(2, n-1, n) == 1
 }
 
-// checkPrime выполняет проверку на простоту: сначала по Ферма, затем перебором.
 func checkPrime(n int) bool {
 	if n == 2 {
 		return true
@@ -45,7 +42,6 @@ func checkPrime(n int) bool {
 	return true
 }
 
-// extendedEuclid возвращает (gcd, x, y) такие, что: a*x + b*y = gcd.
 func extendedEuclid(a, b int) (int, int, int) {
 	if b == 0 {
 		return a, 1, 0
@@ -56,43 +52,33 @@ func extendedEuclid(a, b int) (int, int, int) {
 	return gcd, x, y
 }
 
-// generateOpenKey вычисляет открытый ключ: g^k mod p.
 func generateOpenKey(p, g, k int) int {
 	return modPow(g, k, p)
 }
 
-// encodeBlock шифрует блок m с помощью параметров h, p, g и случайного k.
 func encodeBlock(h, m, p, g, k int) (int, int) {
 	c1 := modPow(g, k, p)
 	c2 := (m * modPow(h, k, p)) % p
 	return c1, c2
 }
 
-// decodeBlock дешифрует блок, вычисляя обратный элемент и восстанавливая исходное сообщение.
 func decodeBlock(x, c1, c2, p int) int {
-	// Возводим c1 в степень закрытого ключа x по модулю p.
 	c1Exp := modPow(c1, x, p)
-	// Вычисляем модульное обратное к c1Exp по модулю p с помощью расширенного алгоритма Евклида.
 	_, _, inv := extendedEuclid(p, c1Exp)
-	// Приводим к положительному остатку.
 	inv = ((inv % p) + p) % p
 	result := (c2 * inv) % p
 	return result
 }
 
-// readFileBytes читает содержимое файла и возвращает его как срез байт.
 func readFileBytes(filename string) ([]byte, error) {
 	return ioutil.ReadFile(filename)
 }
 
-// writeFileBytes записывает данные в файл.
 func writeFileBytes(filename string, data []byte) error {
 	return ioutil.WriteFile(filename, data, 0644)
 }
 
-// encrypt принимает исходный срез байт, шифрует его и возвращает зашифрованные числа и размер блока.
 func encrypt(byteString []byte, key, p, g int) ([]int, int) {
-	// Вычисляем размер блока: ((p-2).bit_length() - 1) // 8.
 	bitLen := 0
 	temp := p - 2
 	for temp > 0 {
@@ -104,7 +90,6 @@ func encrypt(byteString []byte, key, p, g int) ([]int, int) {
 		lenMsg = 1
 	}
 
-	// Добавляем разделитель (255) и дополняем нулями до кратности lenMsg.
 	byteString = append(byteString, 255)
 	padding := lenMsg - (len(byteString) % lenMsg)
 	for i := 0; i < padding; i++ {
@@ -112,13 +97,11 @@ func encrypt(byteString []byte, key, p, g int) ([]int, int) {
 	}
 
 	result := []int{}
-	// Шифруем блок за блоком.
 	for i := 0; i < len(byteString); i += lenMsg {
 		blockInt := 0
 		for j := 0; j < lenMsg; j++ {
 			blockInt = (blockInt << 8) | int(byteString[i+j])
 		}
-		// Генерируем случайное k в диапазоне [2, p-2].
 		k := rand.Intn(p-3) + 2
 		c1, c2 := encodeBlock(key, blockInt, p, g, k)
 		fmt.Printf("block=%d\nk=%d\n", blockInt, k)
@@ -127,7 +110,6 @@ func encrypt(byteString []byte, key, p, g int) ([]int, int) {
 	return result, lenMsg
 }
 
-// decrypt принимает зашифрованные данные, дешифрует их и возвращает срез исходных чисел.
 func decrypt(byteString []byte, key, p, g int) []int {
 	bitLen := 0
 	temp := p - 2
@@ -140,7 +122,6 @@ func decrypt(byteString []byte, key, p, g int) []int {
 		lenMsg = 1
 	}
 	result := []int{}
-	// Каждый зашифрованный блок занимает 2*(lenMsg+1) байт.
 	blockSize := 2 * (lenMsg + 1)
 	for i := 0; i < len(byteString); i += blockSize {
 		c1 := 0
@@ -158,7 +139,6 @@ func decrypt(byteString []byte, key, p, g int) []int {
 	return result
 }
 
-// writeIntsToFile записывает срез целых чисел в файл, каждое число представляется в виде lenMsg байт (big-endian).
 func writeIntsToFile(result []int, filename string, lenMsg int) error {
 	data := []byte{}
 	for _, num := range result {
@@ -175,7 +155,6 @@ func writeIntsToFile(result []int, filename string, lenMsg int) error {
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	// Параметры шифрования
 	p := 293
 	g := 4
 	closedKey := 5
@@ -194,7 +173,6 @@ func main() {
 
 	switch mode {
 	case 1:
-		// Шифрование
 		inBytes, err := readFileBytes("in.txt")
 		if err != nil {
 			fmt.Println("Ошибка чтения in.txt:", err)
@@ -207,7 +185,6 @@ func main() {
 			fmt.Println("Ошибка записи out_en.txt:", err)
 			return
 		}
-		// Дешифрование
 		encBytes, err := readFileBytes("out_en.txt")
 		if err != nil {
 			fmt.Println("Ошибка чтения out_en.txt:", err)
@@ -220,7 +197,6 @@ func main() {
 			return
 		}
 	case 2:
-		// Генерация открытого ключа
 		fmt.Println("Открытый ключ:", generateOpenKey(p, g, closedKey))
 	default:
 		fmt.Println("Неверный режим")
