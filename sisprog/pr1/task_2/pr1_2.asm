8c322a4cba0bf1d6ff18bed103e40616a1bf6b81
uncom1c <mishartem2004@gmail.com>
2025-12-02 21:12:32 +0300

    Scripted auto-commit on change (2025-12-02 21:12:32) by gitwatch.sh

--- sisprog/pr1/task_2/pr1_2.asm
+++ sisprog/pr1/task_2/pr1_2.asm
@@ -17,27 +17,22 @@ outer:
     cmp r9, r8
     jge done
 
-    xor r10, r10       ; j = 0
+    xor r10, r10
     
 inner:
     cmp r10, r8
     jge next
     
-    ; src[n-1-j][i]
     mov r11, r8
-    dec r11           ; n-1
-    sub r11, r10      ; n-1-j (строка)
+    dec r11
+    sub r11, r10 ; n-1-j
     
-    ; Получаем указатель на строку src[n-1-j]
     mov rax, [rdi + r11*8]  ; rax = src[n-1-j]
     
-    ; Читаем значение src[n-1-j][i]
     mov eax, [rax + r9*4]   ; eax = src[n-1-j][i]
     
-    ; Получаем указатель на строку dst[i]
     mov rcx, [rsi + r9*8]   ; rcx = dst[i]
     
-    ; Записываем в dst[i][j]
     mov [rcx + r10*4], eax  ; dst[i][j] = src[n-1-j][i]
     
     inc r10
